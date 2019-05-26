1b064641b51266a2f4568232aee1b128e1c99e15
cxy592394546 <44917425+cxy592394546@users.noreply.github.com>
2019-05-26 22:10:48 +0800

    Add files via upload

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -83,11 +83,12 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("HelloWorld", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("Map", cocos2d::Rect(0, 0, mediumResolutionSize.width, mediumResolutionSize.height));
 #else
-        glview = GLViewImpl::create("HelloWorld");
+        glview = GLViewImpl::create("Map");
 #endif
         director->setOpenGLView(glview);
+
     }
 
     // turn on display FPS
