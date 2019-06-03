2463128c5c3ed9bf023f6d6d28853e808ca2fc79
cxy592394546 <44917425+cxy592394546@users.noreply.github.com>
2019-06-03 21:51:20 +0800

    Add files via upload

---
+++ Resources/Map/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Map" tilewidth="32" tileheight="32" tilecount="5625" columns="75">
+ <image source="fc1f4134970a304e2d0239ddd3c8a786c9175cf0.png" width="2400" height="2400"/>
+</tileset>
