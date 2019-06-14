d9c2a20c8b977d87bc6d556606d007f1f7b23406
cxy592394546 <44917425+cxy592394546@users.noreply.github.com>
2019-06-14 18:13:03 +0800

    Add files via upload

---
+++ Classes/Tower.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include"cocos2d.h"
+
+USING_NS_CC;
+
+#define ATTACK_RADIUS 90
+#define ATK_DAMAGE 250
+#define HP 8000
+#define ATTACK_SPEED 100
+
+class Tower
+{
+public:
+	//阵营
+	void setColor(int color) { _color = color; }
+	int getColor() { return _color; }
+	//攻击范围
+	void setAttackRadius(float radius) { attackRadius = radius; }
+	int getAttackRadius() { return attackRadius; }
+	//攻击速度
+	void setAttackInterval(int attackInterval) { attackSpeed = attackInterval; }
+	int getAttackInterval() { return attackSpeed; }
+	//当前生命
+	float& towerHP;
+	static void minusBlood(int damageNum);
+
+	//是否存在
+	bool destoryed();
+	
+
+private:
+	int _color;
+	float damage = ATK_DAMAGE;
+	float attackRadius;
+
+	float originBlood = HP;
+
+	int deathRewardMoney;
+	int deathRewardExp;
+	int attackSpeed = ATTACK_SPEED;
+
+	void initTower();
+};
+
